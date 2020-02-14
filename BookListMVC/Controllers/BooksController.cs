8d9c2880f3cff2e717876a19b67b85cd55a1dcb4
rakeshniraula <rakeshhniraulaa@gmail.com>
2020-02-14 23:20:19 +0545

    Add project files.

---
+++ BookListMVC/Controllers/BooksController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+            {
+                // create
+                return View(Book);
+            }
+            // update
+            Book = _db.Books.FirstOrDefault(u => u.Id == id);
+            if (Book == null)
+            {
+                return NotFound();
+            }
+
+            return View(Book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if(Book.Id == 0)
+                {
+                    // create
+                    await _db.Books.AddAsync(Book);
+                } else
+                {
+                    _db.Books.Update(Book);
+                }
+
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(Book);
+        }
+
+        #region API Calls
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if(bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
+
+
+
+/*
+ * private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromdb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if(bookFromdb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleteing" });
+            }
+
+            _db.Book.Remove(bookFromdb);    
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+ * 
+ * 
+ * */
---
+++ BookListMVC/wwwroot/js/bookList.js
@@ -0,0 +1,85 @@
+﻿
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#dt_load').DataTable({
+        "ajax": {
+            "url": "/Books/GetAll",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "id", "width": "5%" },
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
+            { "data": "price", "width": "10%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/Books/Upsert?id=${data}" class='btn btn-warning text-white' style='cursor:pointer; width:70px;'>
+                            Edit
+                        </a>
+                        &nbsp;
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
+                            onclick=Delete('/books/Delete?id='+${data})>
+                            Delete
+                        </a>
+                        </div>`;
+                }, "width": "50%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
+
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "500",
+    "timeOut": "2000",
+    "extendedTimeOut": "500",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you Sure?",
+        text: "Record will be Deleted Permanently",
+        icon: "warning",
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
+
